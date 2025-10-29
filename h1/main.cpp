6f9e94c910f711535a8569fea135b6e7c549477b
jry2014 <xzpelaaja966@gmail.com>
2025-10-29 20:18:30 +0200

    H1 ready

--- h1/main.cpp
+++ h1/main.cpp
@@ -10,10 +10,19 @@ void divSum (int a, int b){
         double div = 1.0 * a/b;
         cout << "Osamaara on:" << div << endl;
     }else{
-        cout << "virhe:::;" << endl;
+        cout << "virhe: Nollalla ei voi jakaa" << endl;
     }
 }
+int retSum(int a, int b){
+    return a+b;
+}
 
+float retDiv(int a, int b) {
+    if(b == 0){
+        throw runtime_error("jakaja ei saa olla nolla.");
+    }
+    return 1.0 * a / b;
+}
 int main(){
     int a,b;
     cout << "Luku 1" << endl;
@@ -21,8 +30,18 @@ int main(){
     cout << "Luku 2" << endl;
     cin >> b;
     
-
     //Func
     calcSum(a,b);
     divSum(a,b);
+    int sumResult = retSum(a, b);
+    float divResult;
+
+     //jos try-lohkossa nousee runtime error, hypätään catch-lohkoon
+    try {
+        divResult = retDiv(a,b);
+        cout << a << "/" <<  b << " = " << divResult << endl;
+    } catch (runtime_error& e) {
+        cout<<"Error: "<< e.what()<< endl;
+    }
+    return 0;
 }
